6acdb7d7ac36db20012c2ff98f9de43017f33f45
Bima Tribuana Putra <bima.bca23@gmail.com>
2024-12-28 18:13:16 +0000

    Create navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import Navbar from "./Components/Navbar";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>GAC KEY PROJECT</h1>
+      <Navbar />
     </div>
   );
 }
